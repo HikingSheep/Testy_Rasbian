f327be0ff0378cd786db673aa9f95ea7edc63709
Hopu <adomkov@gmail.com>
2019-12-07 23:51:33 +0000

    Finished file system

--- teleBot/main.py
+++ teleBot/main.py
@@ -17,6 +17,7 @@ bot.
 
 import logging
 import os
+import string
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from spotify import get_playlist, get_track
@@ -44,14 +45,18 @@ def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help! \n'
 +'/yt + URL/keywords - play a song from youtube \n'
++'/yt_playlist + URL [under_dev] - play a playlist from youtube \n'
 +'/stop - stop audio playback \n'
 +'/pasue - pause audio playback \n'
 +'/cont - continue audio playback \n'
 +'/volume + INT (e.g. 50 = 50%) - change volume to INT \n'
 +'/cur_volume - get current volume\n'
 +'/play + URL - play audio from any website/link \n'
-#+'/sp + keywords - find a track on spotify \n'
++'/sp + keywords [under_dev] - find a track on spotify \n'
 +'/sp_playlist + keywords - find a playlist on spotify \n \n'
++'/ls - list available file system directories \n'
++'/ls + available folder name - list files in the specified folder \n'
++'/dw + /location/file name - download specified file \n \n'
 +'**/play function was tested only on youtube, vk, vimeo \n'
 +'Since Spotfiy has a lot of restrictions, it allows playback only through certified applications and Web Player \n \n'
 +'***Drop images, video or documents in chat to upload them to the host machine \n')
@@ -60,7 +65,7 @@ def youtube_play(update, context):
     song_name = open(saddness.temp + '/temp/name.txt', 'r') 
     os.system('pkill mpv')
     URL = update.message.text.strip("/yt ").replace(" ","").encode('utf-8')
-    os.system('$(youtube-dl -o - ytsearch:' + URL + '| mpv --no-video - >/dev/null 2>&1 &)'
+    os.system('$(youtube-dl -f worst -o - ytsearch:' + URL + '| mpv --no-video - >/dev/null 2>&1 &)'
 +' && youtube-dl --get-title ytsearch:' + URL + ' > ' + saddness.temp + '/temp/name.txt' 
 +' && youtube-dl --get-id ytsearch:' + URL + ' >> ' + saddness.temp + '/temp/name.txt')
     update.message.reply_text('Playing... \n' + song_name.readline() + '\n' + 'https://youtu.be/' + song_name.readline())
@@ -73,7 +78,7 @@ def youtube_play_playlist(update, context):
     os.system('youtube-dl --get-title '+ URL +' > ' + saddness.temp + '/temp/name.txt')
     update.message.reply_text('Playing playlist... \n' + song_name.read())
     song_name.close()
-    os.system('youtube-dl --skip-unavailable-fragments --yes-playlist -i -o - '+ URL +' | mpv --no-video - ')
+    #os.system('youtube-dl --skip-unavailable-fragments --yes-playlist -i -o - '+ URL +' | mpv --no-video - ')
 
 
 def play_other(update, context):
@@ -102,42 +107,13 @@ def volume(update, context):
 def volume_cur(update, context):
     update.message.reply_text(saddness.vol + '%')
 
-
-def voice(update, context):
-    FileID = update.message.voice.file_id
-    print (FileID)
-    newFile = update.message.voice.get_file()
-    newFile.download(FileID)
-    update.message.reply_text('Hmmm... Uploaded -_-')
-
-def photo(update, context):
-    FileID = update.message.photo[-1].file_id
-    print (FileID)
-    newFile = update.message.photo[-1].get_file()
-    newFile.download(saddness.temp + "/media/images/" + FileID)
-    update.message.reply_text('Hmmm... Uploaded -_-')
-
-def video(update, context):
-    FileID = update.message.video.file_id
-    print (FileID)
-    newFile = update.message.video.get_file()
-    newFile.download(saddness.temp + "/media/video/" + FileID)
-    update.message.reply_text('Hmmm... Uploaded -_-')
-
-def document(update, context):
-    FileID = update.message.document.file_id
-    print (FileID)
-    newFile = update.message.document.get_file()
-    newFile.download(saddness.temp + "/media/documents/" + FileID)
-    update.message.reply_text('Hmmm... Uploaded -_-')
-
 def spotify_playlist(update, context):
     keyword = update.message.text.strip("/sp_playlist ").encode('utf-8')
     update.message.reply_text('Enjoy your playlist! \n' + get_playlist(str(keyword)))
 
 def spotify_track(update, context):
     keyword = update.message.text.strip("/sp ").replace(" ","").encode('utf-8')
-    update.message.reply_text('Enjoy your track! \n' + get_track(str(keyword)))
+    update.message.reply_text('Enjoy your track! \n [under dev]') #+ get_track(str(keyword)))
  
 
 def echo(update, context):
@@ -150,12 +126,56 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
+####File System
+
+def show_media(update, context):
+    keyword = update.message.text.strip("/ls ")
+    os.system('cd '+ saddness.temp + '/media/' + keyword + ' && ls > ' + saddness.temp + '/temp/list.txt')
+    file_list = open(saddness.temp + '/temp/list.txt', 'r')
+    update.message.reply_text(file_list.read())
+    file_list.close()
+
+def download_media(update, context):
+    URL = update.message.text.strip("/dw").replace(" ","").encode('utf-8')
+    update.message.bot.send_document(chat_id = update.message.chat_id, filename=URL, document=open(saddness.temp + '/media' + URL, 'rb'))
+    update.message.reply_text('Downloading from storage... ' + URL)
+
+def voice(update, context):
+    FileID = update.message.voice.file_id[20:]
+    print (FileID)
+    newFile = update.message.voice.get_file()
+    newFile.download(saddness.temp + "/media/audio/" + FileID + '.mp3')
+    update.message.reply_text('Hmmm... Uploaded -_-')
+
+def photo(update, context):
+    FileID = update.message.photo[-1].file_id[40:]
+    print (FileID)
+    newFile = update.message.photo[-1].get_file()
+    newFile.download(saddness.temp + "/media/image/" + FileID + '.jpg')
+    update.message.reply_text('Hmmm... Uploaded -_-')
+
+def video(update, context):
+    FileID = update.message.video.file_id[20:]
+    print (FileID)
+    newFile = update.message.video.get_file()
+    newFile.download(saddness.temp + "/media/video/" + FileID + '.mp4')
+    update.message.reply_text('Hmmm... Uploaded -_-')
+
+def document(update, context):
+    FileID = update.message.document.file_id[20:]
+    print (FileID)
+    newFile = update.message.document.get_file()
+    newFile.download(saddness.temp + "/media/document/" + FileID)
+    update.message.reply_text('Hmmm... Uploaded -_-')
+
+####
+
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("", use_context=True)
+    updater = Updater("991503763:AAH5mpYGYNmblr-p3f715Kt6Foay-JKmQ6Q", use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -175,7 +195,10 @@ def main():
     dp.add_handler(CommandHandler("cont", continue_playback))
 
     dp.add_handler(CommandHandler("sp_playlist", spotify_playlist)) 
-#    dp.add_handler(CommandHandler("sp", spotify_track))
+    dp.add_handler(CommandHandler("sp", spotify_track))
+
+    dp.add_handler(CommandHandler("ls", show_media))
+    dp.add_handler(CommandHandler("dw", download_media))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
--- teleBot/spotify.py
+++ teleBot/spotify.py
@@ -4,6 +4,8 @@ import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 
+SPOTIPY_CLIENT_ID = '5732bebf2ed9470697dd2fb2e159daef'
+SPOTIPY_CLIENT_SECRET = '36fe70e6d78940f79cd67f67c3d4e14c'
 
 
 client_credentials_manager = SpotifyClientCredentials(SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET)
@@ -16,7 +18,7 @@ def get_playlist(keyword):
         if pl['public'] is None or pl['public'] == True:
             return pl['external_urls']['spotify']
 
-#def get_track(keyword):
-#    playlist_list = sp.search(keyword, limit=1, type='track')['items']
+def get_track(keyword):
+    playlist_list = sp.search(keyword, limit=1, type='track')['items']
     
-#    return str(playlist_list)
+    return str(playlist_list)
